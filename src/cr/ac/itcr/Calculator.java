81b449f35434290bacdefe9b0d978ae14786d789
Ignacio Vargas <ignaevc@hotmail.com>
2021-03-24 20:17:59 -0600

    Added basic calculator class

---
+++ src/cr/ac/itcr/Calculator.java
@@ -0,0 +1,23 @@
+package cr.ac.itcr;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a+b;
+    }
+
+    public float sum(float a, float b) {
+        return a+b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public float multiply(float a, float b) {
+        return a*b;
+    }
+
+    public float divide(float a, float b) {
+        return a/b;
+    }
+}
